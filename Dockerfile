c285770e48c807dc3d4583d61b1645956fd31aa9
XinyiWang-Jessica <108918930+XinyiWang-Jessica@users.noreply.github.com>
2023-04-23 15:39:22 -0700

    two dockerfiles

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,22 @@
+
 # Install the base requirements for the app.
 # This stage is to support development.
+
+
+
+FROM python:3.8-slim-buster 
+WORKDIR /Facial_Expression_Identification
+COPY requirements.txt .
+RUN python3 -m pip install --upgrade pip setuptools wheel
+RUN python3 -m pip install -r requirements.txt  
+COPY . .
+# Make port 5000 available to the world outside this container
+EXPOSE 5000
+# Define environment variable
+ENV FLASK_APP=backend.py
+# Run app.py when the container launches
+CMD ["flask", "run", "--host=0.0.0.0"]
+
 
 FROM node:14.17.0-alpine AS base
 # Set the working directory to /app
@@ -8,18 +25,5 @@ COPY package*.json ./
 RUN npm install 
 RUN npm install react-scripts@3.4.1 -g --silent
 COPY . .
-
+EXPOSE 3000
 CMD ["npm", "start"]
-
-
-FROM python:3.8-slim-buster AS base
-WORKDIR /Facial_Expression_Identification
-COPY requirements.txt .
-RUN python3 -m pip install --upgrade pip setuptools wheel
-RUN python3 -m pip install -r requirements.txt  
-
-COPY . .
-
-ENTRYPOINT [ "python" ]
-
-CMD ["backend.py" ]
---
+++ src/.ipynb_checkpoints/App-checkpoint.js
@@ -0,0 +1,91 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  // State to store the uploaded image file
+  const [image, setImage] = useState(null);
+  const [data, setData] = useState({ songs: [], urls: [] });
+  const [isLoading, setIsLoading] = useState(false);
+  const [err, setErr] = useState('');
+
+  // Function to handle image upload
+  const handleImageUpload = (event) => {
+    // Get the uploaded file
+    let image = event.target.files[0];
+
+    // Read the file and convert it to a data URL
+    let reader = new FileReader();
+    reader.onloadend = () => {
+      // Check if reading was successful
+      if (reader.result) {
+        // Success: set the result to the image state variable
+        setImage(reader.result);
+      }
+    };
+    reader.readAsDataURL(image);
+  };
+
+  const handleClick = async () => {
+    setIsLoading(true);
+    try {
+      const formData = new FormData();
+      formData.append('image', image); // Append the image file to FormData
+      console.log('image', image)
+
+      const response = await fetch('http://127.0.0.1:5000/get_recommendations/', {
+        method: 'POST',
+        body: formData, // Set the FormData as the request body
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        // Update state with fetched data
+        setData(data);
+      } else {
+        throw new Error('Failed to fetch data');
+      }
+    } catch (err) {
+      setErr(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+
+  return (
+      <div>
+        <h1>Emotion-Based Playlist Creator</h1>
+        <h2>Please upload a facial image, we would provide a customized playlist to make your day!<br />
+          (We would not save your image and you can remove it at any time.)</h2>
+        <label htmlFor="imageUpload">Upload Image:</label>
+        <input id="imageUpload" type="file" onChange={handleImageUpload} />
+        <br />
+        <div>
+          {image && (
+              <div>
+                <img alt="not found" width="500px" src={image} />
+                <br />
+                <button onClick={() => setImage(null)}>Remove</button>
+              </div>
+          )}
+          <br />
+          <button onClick={handleClick}>Create Playlist</button>
+        </div>
+        <div>
+          {isLoading ? (
+              <p>Loading...</p>
+          ) : (
+              <ul>
+                {data.songs.map((song, index) => (
+                    <li key={song}>
+                      <a href={data.urls[index]}>{song}</a>
+                    </li>
+                ))}
+              </ul>
+          )}
+        </div>
+        {err && <p>Error: {err}</p>}
+      </div>
+  );
+};
+
+export default App;
