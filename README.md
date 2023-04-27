dd5c2c87ac10f8074fa67f9d7e3f8133c671b50f
Xinnnnn <xinnnnn@xinnnnns-mbp.ds.usfca.edu>
2023-04-27 13:43:10 -0700

    add background and improve format

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
 # InMood Facial Expression Identification Engine
 
-The InMood facial expression identification engine is a cutting-edge technology that revolutionizes recommendation by utilizing real-time facial expressions. Powered by a sophisticated convolutional neural network (CNN) algorithm trained on a vast dataset of over 28000 [facial images](https://www.kaggle.com/datasets/astraszab/facial-expression-dataset-image-folders-fer2013) with 7 emotion labels, it accurately analyzes users' facial expressions in their uploaded images to generate personalized music playlists based on their emotions. Its unparalleled level of personalization, emotional resonance, and adaptability make it a superior choice over traditional music recommendation strategies, delivering an exceptional music listening experience for users.
+At the heart of InMood is a cutting-edge facial expression identification engine that revolutionizes music recommendation. Our team developed this technology to provide users with a truly personalized and unique customized experience that goes beyond traditional recommendation strategies. Our engine is powered by a sophisticated convolutional neural network (CNN) algorithm that has been trained on a vast dataset of over 28,000 [facial images](https://www.kaggle.com/datasets/astraszab/facial-expression-dataset-image-folders-fer2013) with 7 emotion labels.
+ 
+The aim of InMood is to provide users with a truly personalized and unique experience that goes beyond traditional recommendation strategies. By harnessing the power of facial expressions as potent indicators of emotions, InMood taps into this rich source of information to curate music playlists, video recommendations, potential loved goods, and more that deeply resonate with users on an emotional level.
 
-**Application Demo Link: [InMood](http://18.119.0.164:3000/)**
 
-## Intuition
-The driving force behind the creation of InMood was to provide users with a truly personalized and unique music experience that transcends traditional music recommendation strategies. Harnessing the power of facial expressions as potent indicators of emotions, this cutting-edge engine taps into this rich source of information to curate music playlists that deeply resonate with users on an emotional level.
+## Application: InMood Emotion-Based Music Playlist Generator
+To showcase the power of InMood, we created a demo that generates customized music playlists based on users' uploaded images, accurately analyzing their facial expressions to determine their emotions.
+InMood's unparalleled level of personalization, emotional resonance, and adaptability makes it a superior choice over traditional music recommendation strategies. Our technology delivers an exceptional music listening experience that truly connects with users on a deep emotional level.
 
-## Usage
+### **Demo Link: [InMood](http://18.119.0.164:3000/)**
+
+![Alt text](./img/step_1.jpg)
+
+### Steps
 Using the InMood facial expression identification engine is remarkably simple and user-friendly.
 
 - Users can effortlessly upload their facial images with a click of a button.
@@ -15,15 +21,24 @@ Using the InMood facial expression identification engine is remarkably simple an
 - Based on the emotions detected, the InMood engine generates a customized music playlist that aligns perfectly with the user's current mood and emotional state.
 - Users can embark on their emotional music journey, carefully curated to evoke specific emotions and enhance their overall music listening experience.
 
-## Superiority
+### Examples
+Example 1:
+![Alt text](./img/step_2.jpg)
+
+Example 2:
+![Alt text](./img/step_3.jpg)
+
+### Superiority
 The InMood facial expression identification engine boasts numerous advantages over traditional music recommendation strategies.
 
 - It delivers a highly personalized and emotionally resonant music experience by taking into account the user's current emotional state based on their facial expressions.
 - By analyzing facial expressions in real-time, it captures the user's ever-changing emotional state throughout the day, generating dynamic and adaptive music playlists accordingly, providing frequent and tailored playlists as desired.
 - Furthermore, the InMood engine can be seamlessly integrated into a wide range of scenarios, such as during workouts, relaxation sessions, or social gatherings, where users may have varying emotional states and music preferences.
 
+
 ## Cooperation
 We are open to collaboration with other content platforms. The InMood facial expression identification engine can be easily integrated into existing music streaming platforms or apps, augmenting their recommendation capabilities and providing a unique competitive advantage.
+
 
 ## Team
 - Yichen Zhao is responsible for CNN model development, training, and deployment.
--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
 import Paper from '@mui/material/Paper';
-import { useTheme, createTheme } from '@mui/material/styles';
-import styled from '@mui/material/styles/styled';
 import './App.css';
 import logo from './logo.jpg';
+import background from './background.jpg';
 import { Button, Stack, Grid, Box, List, ListItem, ListItemText, Link } from '@mui/material';
 
 
@@ -13,7 +12,6 @@ const App = () => {
   const [data, setData] = useState({ songs: [], artists: [], urls: [], imgs:[] });
   const [isLoading, setIsLoading] = useState(false);
   const [err, setErr] = useState('');
-  const theme = useTheme();
 
   // Function to handle image upload
   const handleImageUpload = (event) => {
@@ -39,7 +37,7 @@ const App = () => {
       formData.append('image', image); // Append the image file to FormData
       console.log('image', image)
 
-      const response = await fetch('http://18.119.0.164:5000/get_recommendations/', {
+      const response = await fetch('http://localhost:5000/get_recommendations/', {
         method: 'POST',
         body: formData, // Set the FormData as the request body
       });
@@ -58,23 +56,20 @@ const App = () => {
     }
   };
 
-  const Item = styled(Paper)(({ theme }) => ({
-    ...theme.typography.body2,
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-    height: 60,
-    lineHeight: '60px',
-  }));
 
   return (
-      <div style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh',
-        background: '#f7f7f7',
-        position: 'relative',
-      }}>
+      <Paper
+          sx={{
+            backgroundImage: `url(${background})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+            minHeight: '100vh',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'column',
+          }}
+      >
         <div style={{
           background: '#fff',
           padding: '40px',
@@ -82,76 +77,100 @@ const App = () => {
           boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)',
           position: 'relative',
         }}>
-          <div style={{
-            position: 'absolute',
-            top: '16px',
-            left: '10%',
-            transform: 'translateX(-50%)',
-            zIndex: 1,
-
-          }}>
+        <div style={{
+          position: 'absolute',
+          top: '5px',
+          left: '10%',
+          transform: 'translateX(-50%)',
+          zIndex: 1,
+        }}>
             <img src={logo} alt="logo" style={{ width: '190px' }} />
           </div>
           <h1 style={{ fontSize: '48px', fontWeight: 'bold', marginBottom: '20px' }}>InMood Emotion-Based Recommendation Engine</h1>
+          <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
           <h4 style={{ fontSize: '18px', lineHeight: '1.5', marginBottom: '20px' }}>
             Please upload a facial image (JPG or PNG), we would provide a customized playlist to make your day!
             <br/>
             We would not save your image and you can remove it at any time.
           </h4>
-        <Stack direction="row" spacing={5}>
-          <label htmlFor="imageUpload">
-            <Button variant="contained" size="medium" component="label" onClick={() => document.getElementById('imageUpload').click()}>Upload Image</Button>
-            <input id="imageUpload" type="file" onChange={handleImageUpload} style={{ display: 'none' }} />
-          </label>
-          <Button variant="contained" size="medium" onClick={handleClick} disabled={!image}>
-            Create Playlist
-          </Button>
-        </Stack>
-
-        <br />
-        <div>
-          {image && (
-              <div>
-                <img alt="not found" width="500px" src={image} />
-                <br />
-                <Button size="small" onClick={() => setImage(null) & setData({ songs: [], artists: [], urls: [], imgs:[] })} style={{ opacity: 0.5 }}>
-                  Remove
+          </Box>
+          <br/>
+          <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+            <Stack direction="row" spacing={5}>
+              <label htmlFor="imageUpload">
+                <Button variant="contained" size="medium" component="label" sx={{ marginRight: '16px' }} onClick={() => document.getElementById('imageUpload').click()}>
+                  Upload Image
                 </Button>
-              </div>
-          )}
-          <br />
-          {isLoading ? (
-              <p>Loading...</p>
-          ) : (
-              <Grid container>
-                <Grid item xs={10}>
-                  <List>
-                    {data.songs.map((song, index) => (
-                        <ListItem key={song}>
-                          <ListItemText
-                              primary={
-                                <Link href={data.urls[index]} target="_blank">
-                                  {song}
-                                </Link>
-                              }
-                              secondary={data.artists[index]}
-                          />
-                        </ListItem>
-                    ))}
-                  </List>
-                </Grid>
-                <Grid item xs={10}>
-                  {data.songs && data.songs.length>0 && (
-                  <Button variant="contained" size="medium" onClick={handleClick} disabled={!image}>
-                    Try Some New
-                  </Button>)}
-                </Grid>
-              </Grid>
-          )}
+                <input id="imageUpload" type="file" onChange={handleImageUpload} style={{ display: 'none' }} />
+              </label>
+              <Button variant="contained" size="medium" sx={{ marginRight: '16px' }} onClick={handleClick} disabled={!image}>
+                Create Playlist
+              </Button>
+            </Stack>
+          </Box>
+        <br />
+
+        <div>
+          <Grid container spacing={0}>
+            <Grid item xs={5.5} >
+              <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}} >
+                {image && (
+                    <div>
+                      <img alt="not found" width="500px" src={image} />
+                      <br />
+                      <Button size="small" onClick={() => setImage(null) & setData({ songs: [], artists: [], urls: [], imgs:[] })} style={{ opacity: 0.5 }}>
+                        Remove
+                      </Button>
+                    </div>
+                )}
+              </Box>
+            </Grid>
+            <Grid item xs={5.5}>
+              <br />
+              {isLoading ? (
+                  <p>Loading...</p>
+              ) : (
+                  <Grid container sx={{ justifyContent: 'center', alignItems: 'center' }}>
+                    <Grid item xs={8.5}>
+                      <List>
+                        {data.songs.map((song, index) => (
+                            <ListItem key={song}>
+                              <ListItemText
+                                  primary={
+                                    <Link href={data.urls[index]} target="_blank">
+                                      {song}
+                                    </Link>
+                                  }
+                                  secondary={data.artists[index]}
+                              />
+                            </ListItem>
+                        ))}
+                      </List>
+                    </Grid>
+                  </Grid>
+              )}
+            </Grid>
+          </Grid>
+
+          <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}} >
+            <Stack direction="row" spacing={5}>
+              {data.songs && data.songs.length>0 && (
+                  <div>
+                    <Button variant="contained" size="medium" sx={{ marginRight: '16px' }} onClick={() => setImage(null) & setData({ songs: [], artists: [], urls: [], imgs:[] })}>
+                      Re-upload image
+                    </Button>
+                  </div>
+              )}
+              {data.songs && data.songs.length>0 && (
+              <Button variant="contained" size="medium" sx={{ marginRight: '16px' }} onClick={handleClick} disabled={!image}>
+                Try New Playlist
+              </Button>)}
+            </Stack>
+          </Box>
         </div>
           {err && <p>Error: {err}</p>}
         </div>
-      </div>
+      </Paper>
   );
 }
 
