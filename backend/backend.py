d1d162b94fb936fae5da74366ba6125523112089
Xinnnnn <xinnnnn@Xinnnnns-MacBook-Pro.local>
2023-05-02 18:48:54 -0700

    convert to production

--- backend/backend.py
+++ backend/backend.py
@@ -46,4 +46,4 @@ def get_recommendations():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ const App = () => {
       formData.append('image', image); // Append the image file to FormData
       console.log('image', image)
       // https://inmood-backend.netlify.app/
-      const response = await fetch('http://127.0.0.1:5000', {
+      const response = await fetch('http://0.0.0.0:5000', {
         method: 'POST',
         body: formData, // Set the FormData as the request body
       });
